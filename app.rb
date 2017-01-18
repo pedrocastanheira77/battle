e82f430da011fe6d89d68fe21046d6ab9d4aeba1
Rick Clark <rkingsleyclark@gmail.com>
2017-01-18 13:45:44 +0000

    settng session secret

--- app.rb
+++ app.rb
@@ -2,15 +2,31 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
-  set :session, true
+  enable :sessions
+  set :session_secret, "secret"
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @p1_name = params[:p1_name]
-    @p2_name = params[:p2_name]
+    p "in /names"
+    p params
+    p session
+    p 'now assigning session variables'
+    session[:p1_name] = params[:p1_name]
+    session[:p2_name] = params[:p2_name]
+    p session
+    p session[:p1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    p "now in /play"
+    p session
+    p session[:p1_name]
+    @p1_name = session[:p1_name]
+    @p2_name = session[:p2_name]
     erb(:play)
   end
 end
