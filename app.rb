4c70d8bdc1fd0a69aa8affc15693b5d7d6e6eb05
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-01-18 12:24:24 +0000

    insert player 1 and 2 and submit

--- app.rb
+++ app.rb
@@ -3,9 +3,14 @@ require 'shotgun'
 
 class Battle < Sinatra::Base
   set :session, true
-  
+
   get '/' do
-    'Hello'
+    erb(:index)
   end
 
+  post '/names' do
+    @p1_name = params[:p1_name]
+    @p2_name = params[:p2_name]
+    erb(:play)
+  end
 end
---
+++ spec/features/battle_spec.rb
@@ -0,0 +1,11 @@
+describe Battle do
+  feature 'let players enter their name' do
+     scenario 'user fills in name form' do
+       visit '/'
+         fill_in('p1_name', with: 'Rick')
+         fill_in('p2_name', with: 'Pedro')
+         click_button('submit_players')
+      expect(page).to have_text("Rick and Pedro have entered the game")
+     end
+   end
+ end
